cefa884443d634f3f94f6874d0575ef456055ba9
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-24 13:22:29 -0500

    problem 136 solution upload

---
+++ Problem_List/136.Single_Number/code.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+using namespace std;;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int num : nums){
+            ans ^= num;
+        }
+        return ans;
+    }
+};
