a6914df105f3752b93ba79fa9ca2b9ef0f3b532c
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-24 20:34:32 -0500

    problem 191 solution upload

---
+++ Problem_List/191.Number_of_1_Bits/code.cpp
@@ -0,0 +1,13 @@
+using namespace std;;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0){
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
