518ad701ae23ef32e1d4ff42665b8a07decc077a
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-24 19:05:09 -0500

    problem 190 solution upload

---
+++ Problem_List/190.Reverse_Bits/code.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+using namespace std;;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+
+        for(int i = 0; i < 32; i++){
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+
+        return result;
+    }
+};
