80519b91bdd8165abba428c7202a0c6a341b6482
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-27 21:23:08 -0500

    problem 1071 solution upload

---
+++ Problem_List/1071.Greatest_Common_Divisor_of_Strings/code.cpp
@@ -0,0 +1,10 @@
+#include<String>
+#include <numeric> 
+using namespace std;;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return str1 + str2 != str2 + str1 ? "" : str1.substr(0, gcd(str1.length(), str2.length()));
+    }
+};
