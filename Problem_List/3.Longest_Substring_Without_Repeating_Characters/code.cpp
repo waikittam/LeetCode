95e1db9338ecc4ff59d5b5a07e9bb9beb70de22a
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-28 00:05:21 -0500

    solution update

--- Problem_List/3.Longest_Substring_Without_Repeating_Characters/code.cpp
+++ Problem_List/3.Longest_Substring_Without_Repeating_Characters/code.cpp
@@ -1,22 +1,21 @@
-#include <iostream> 
-#include <unordered_set>
-
-using namespace std;
+#include<unordered_set>
+#include<String>
+using namespace std;;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> charSet;
-        int left = 0;
+        unordered_set<char> set;
         int maxLength = 0;
+        int left = 0;
 
-        for (int right = 0; right < s.length(); ++right) {
-            while (charSet.find(s[right]) != charSet.end()) {
-                charSet.erase(s[left]);
+        for(int x = 0; x < s.length(); x++){
+            while(set.find(s[x]) != set.end()){
+                set.erase(s[left]);
                 left++;
             }
-            charSet.insert(s[right]);
-            maxLength = max(maxLength, right - left + 1);
+            set.emplace(s[x]);
+            maxLength = max(maxLength, (x + 1 - left));
         }
 
         return maxLength;
