058ea0612849fd6bce758b2ceef52a21a93d9649
Wai Kit Tam <ricky19971113@gmail.com>
2024-11-30 18:06:50 -0500

    problem 33 solution upload

---
+++ Problem_List/33.Search_in_Rotated_Sorted_Array/code.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+using namespace std;;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, mid;
+
+        while(left <= right){
+            mid = left + (right - left)/2; 
+            if(target == nums[mid])
+                return mid;
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }else{
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
