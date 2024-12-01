80bff0c2de2b73f855c494d6b9b4c764965a6c32
Wai Kit Tam <ricky19971113@gmail.com>
2024-12-01 04:05:35 -0500

    problem 73 solution upload

---
+++ Problem_List/73.Set_Matrix_Zeroes/code.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+using namespace std;;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRow = false, firstCol = false;
+
+        for(int row = 0; row < m; row++){
+            if(matrix[row][0] == 0)
+                firstCol = true;
+        }
+        for(int col = 0; col < n; col++){
+            if(matrix[0][col] == 0)
+                firstRow = true;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if(firstCol){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(firstRow){
+            for(int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
