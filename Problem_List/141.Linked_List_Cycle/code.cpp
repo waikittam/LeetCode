0a52d3506485129ba0d4c6501f6576b47f9e3529
Wai Kit Tam <ricky19971113@gmail.com>
2024-12-27 09:22:54 -0500

    problem 141 solution upload

---
+++ Problem_List/141.Linked_List_Cycle/code.cpp
@@ -0,0 +1,27 @@
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr) return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
