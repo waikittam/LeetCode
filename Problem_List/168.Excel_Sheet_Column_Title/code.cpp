7ab072d2d8b0a19fa3a241fdc4595b906183447a
Wai Kit Tam <ricky19971113@gmail.com>
2024-12-28 17:18:24 -0500

    problem 168 solution upload

---
+++ Problem_List/168.Excel_Sheet_Column_Title/code.cpp
@@ -0,0 +1,17 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+
+        while(columnNumber > 0){
+            columnNumber--;
+            result = char(columnNumber % 26 + 'A') + result;
+            columnNumber /= 26;
+        }
+        
+        return result;
+    }
+};
